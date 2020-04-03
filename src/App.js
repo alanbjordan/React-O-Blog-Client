bd2ed102885b3610dfc5978e10441bc2a47e52fa
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-03 00:48:34 -0400

    -Removed starter code -Added Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import BlogsList from './components/BlogsList';
+import BlogPost from './components/BlogPost';
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path='/' component={BlogsList} />
+        <Route path='/posts/:user_post?' component={BlogPost} />
+      </Router>
     </div>
   );
 }
