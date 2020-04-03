70114e47e7fec8fb235d797c05555a15a56bf67d
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-03 14:49:38 -0400

    Refactoring -Remove console logs -Add link to comments route

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import BlogComment from './BlogComment';
 import './blogs.css';
 
 class BlogsList extends Component {
@@ -29,15 +31,14 @@ class BlogsList extends Component {
     render() {
         if (this.state.data != 0) {
             const prop = this.props.match.params.user_post;
-        const blogsData = this.state.data;
-        console.log("On the route blog data", blogsData)
+            const blogsData = this.state.data;
         return (
             <div className="blogPost">
                 <h1>{blogsData[`${prop}`].title}</h1><br></br> <hr />
                 <div className="textWrapper">
                     <h5 className="blogText">{blogsData[`${prop}`].blog_posts}</h5>                     
                 </div>
-
+                <Link to={`/posts/comments/${prop}`}>Read Comments</Link>
             </div>
         )
         } else {
